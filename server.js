88ad8bdac8f1ed0c399e669522d077b85d03462b
he3 <henrys@gmail.com>
2016-04-03 12:51:20 -0400

    Test CI

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
   console.log("handled request");
-  response.end("Hello World!");
+  response.end("Hello World! yay changed");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
-console.log("running on port 8080");
+console.log("running on port 8080! yay changed");
