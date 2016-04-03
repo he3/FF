b2a4069b5200aeffff7998d9f5ef0e9e5bbe3131
he3 <henrys@gmail.com>
2016-04-03 15:01:59 -0400

    Express Added

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-var http = require('http');
-var handleRequest = function(request, response) {
-  response.writeHead(200);
-  console.log("handled request");
-  response.end("Hello World! yay changed");
-}
-var www = http.createServer(handleRequest);
-www.listen(8080);
-console.log("running on port 8080! yay changed");
+"use strict"
+
+const express = require("express");
+
+const PORT = 8080;
+
+const app = express();
+app.get("/", function(req, res){
+    console.log("Request handled.")
+    res.send("Express on"); 
+});
+
+app.listen(PORT);
+console.log("Running on http://localhost:" + PORT);
